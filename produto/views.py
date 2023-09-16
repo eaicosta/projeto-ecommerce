9a7eba761c6f40d88c0a2f5a9eaec4146970daf1
Davi Costa <davicostadelira04@gmail.com>
2023-09-16 14:04:30 -0300

    Listando produtos(template)

--- produto/views.py
+++ produto/views.py
@@ -3,12 +3,16 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.list import ListView
 
+from . import models
+
 # Create your views here.
 
 
 class ListaProdutos(ListView):
-    def get(self, *args, **kwargs):
-        return HttpResponse('ListaProdutos')
+    model = models.Produto
+    template_name = 'produto/lista.html'
+    context_object_name = 'produtos'
+    paginate_by = 10
 
 
 class DetalheProduto(View):
