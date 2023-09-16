2dba8284d9016998663d784ff2bb0dd33ee1647b
Davi Costa <davicostadelira04@gmail.com>
2023-09-16 12:24:47 -0300

    URLS

--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,36 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.views.generic.list import ListView
 
 # Create your views here.
+
+
+class ListaProdutos(ListView):
+    def get(self, *args, **kwargs):
+        return HttpResponse('ListaProdutos')
+
+
+class DetalheProduto(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('DetalheProduto')
+
+
+class AdicionarAoCarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('AdicionarAoCarrinho')
+
+
+class RemoverDoCarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('RemoverDoCarrinho')
+
+
+class Carrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Carrinho')
+
+
+class Finalizar(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Finalizar')
